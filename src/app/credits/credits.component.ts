93e2efcc3a8f9dc89e13ab4bb747bd021194abed
Shyam2101 <shiyamsundhar2101@gmail.com>
2024-04-12 17:36:04 +0530

    CHANGES DONE

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-credits',
   standalone: true,
   imports: [],
   templateUrl: './credits.component.html',
-  styleUrl: './credits.component.scss'
+  styleUrl: './credits.component.scss',
+  encapsulation:ViewEncapsulation.None
+,
 })
 export class CreditsComponent {
 
